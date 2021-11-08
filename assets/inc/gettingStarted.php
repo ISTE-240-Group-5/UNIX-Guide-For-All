e9cb9f590cc893e3189a3df1a413bde23dfd9cd8
zoryisnotreal <90271082+zoryisnotreal@users.noreply.github.com>
2021-11-08 01:16:32 -0500

    Update gettingStarted.php

---
+++ assets/inc/gettingStarted.php
@@ -0,0 +1,49 @@
+ <!-- 
+Name         : gettingstarted
+File         : gettingstarted.php
+Objectives   : _________________________________________
+Usage        : Read directly in browser.
+Compatibility: caniuse.com, allmost all browsers.
+Rights       : Copyright (c) 2021 Quasar - GPLv3             
+-->
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Quasar-edu.page</title>
+    <meta name="_____" content="_____">
+    <meta name="_____" content="_____">
+
+    <!-- Mobile-friendly viewport -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+</head>
+<body>
+
+<!-- Include for nav... -->
+   <?php 
+   	
+   	require //nav
+   ?>
+    
+    <h1> UNIX 101: A Survival Guide </h1>
+    
+    <p>
+    
+    <!-- Initial content -->
+    
+    </p>
+    
+    <?php 
+    //include the rest of getting started
+    ?>
+
+    <footer>
+        <p>Copyright <time datetime="2021">2015</time> ____</p>
+    </footer>
+
+    <script src="js/scripts.js?v=1.0"></script>
+</body>
+</html>
