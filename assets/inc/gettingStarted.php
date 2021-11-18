4654e47b0f9057e0cead7bace990e3904e98c920
zoryisnotreal <90271082+zoryisnotreal@users.noreply.github.com>
2021-11-18 11:09:09 -0500

    Adding php

--- assets/inc/gettingStarted.php
+++ assets/inc/gettingStarted.php
@@ -25,34 +25,42 @@ Rights       : Copyright (c) 2021 Quasar - GPLv3
 	<div class="content">
     <h1 class="header"> UNIX 101: A Survival Guide </h1>
     
-    <p>
-
-    </p>
-    
-    
+   <?php
+   
+   /*
+   require 'gettingStartedSubDir/whatIsLinuxnix.php';
+   
+   require 'gettingStartedSubDir/howToUseUnix.php';
+   
+   require 'gettingStartedSubDir/howToTerm.php';
+   */
+   
+   ?>
+   
     <button onclick="topFunction()" id="myBtn" title="Go to top">Back to top</button>
     
     </div>
+   
     <script>
 	//Get the button
 	var mybutton = document.getElementById("myBtn");
 
-	// When the user scrolls down 20px from the top of the document, show the 		button
+	// When the user scrolls down 20px from the top of the document, show the button
 	window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+	function scrollFunction() {
+ 		 if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+   		 mybutton.style.display = "block";
+  		} else {
+  		  mybutton.style.display = "none";
+  		}
+	}
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+	// When the user clicks on the button, scroll to the top of the document
+	function topFunction() {
+ 		 document.body.scrollTop = 0;
+ 		 document.documentElement.scrollTop = 0;
+	}
 </script>
 
 </body>
