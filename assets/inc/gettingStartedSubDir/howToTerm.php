a4e8e5614f5f4efc566168b047a70ec679d56cec
zoryisnotreal <90271082+zoryisnotreal@users.noreply.github.com>
2021-11-19 13:31:09 -0500

    changes

--- assets/inc/gettingStartedSubDir/howToTerm.php
+++ assets/inc/gettingStartedSubDir/howToTerm.php
@@ -1,25 +1,4 @@
-<!-- 
-Name         : index
-File         : index.html
-Objectives   : _________________________________________
-Usage        : Read directly in browser.
-Compatibility: caniuse.com, allmost all browsers.
-Rights       : Copyright (c) 2021 Quasar - GPLv3             
--->
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Quasar</title>
-    <meta name="_____" content="_____">
-    <meta name="_____" content="_____">
-
-    <!-- Mobile-friendly viewport -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/stylesheet.css">
-</head>
-<body>
 
 <h2 class="subheader"> How to use Terminal </h2>
 
--- assets/inc/gettingStartedSubDir/howToUseUnix.php
+++ assets/inc/gettingStartedSubDir/howToUseUnix.php
@@ -1,25 +1,4 @@
-<!-- 
-Name         : index
-File         : index.html
-Objectives   : _________________________________________
-Usage        : Read directly in browser.
-Compatibility: caniuse.com, allmost all browsers.
-Rights       : Copyright (c) 2021 Quasar - GPLv3             
--->
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Quasar</title>
-    <meta name="_____" content="_____">
-    <meta name="_____" content="_____">
-
-    <!-- Mobile-friendly viewport -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/stylesheet.css">
-</head>
-<body>
 
 <h2 class="subheader"> How to use Unix </h2>
 
@@ -28,9 +7,3 @@ Consectetur minim laboris sunt consectetur ea excepteur laboris nisi culpa culpa
 Consectetur minim laboris sunt consectetur ea excepteur laboris nisi culpa culpa pariatur enim. Sit dolor veniam reprehenderit non est eu voluptate proident eu et in commodo ea. Commodo aliquip officia duis non aliquip elit duis nisi. Aute anim et voluptate Lorem dolore dolore esse cupidatat enim laboris magna. Do tempor adipisicing dolor aliqua eu cupidatat id ea eiusmod culpa. Laboris voluptate id amet cupidatat commodo aliquip et ad sunt ullamco aliquip commodo veniam nostrud. Do ut tempor consectetur dolor velit officia aliqua dolore proident dolore magna eu dolor. Magna ea cillum occaecat 
 </p>
 
-
-
-
-    <script class="js/scripts.js?v=1.0"></script>
-</body>
-</html>
