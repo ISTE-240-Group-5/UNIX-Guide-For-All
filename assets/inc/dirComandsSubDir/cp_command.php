8ae734429bed46ec63c9700d1673ae7a5412b770
Joshua Kelly <jmk3378@rit.edu>
2021-11-22 15:12:06 -0500

    Added PHP and HTML template to pages.

---
+++ assets/inc/dirComandsSubDir/cp_command.php
@@ -0,0 +1,29 @@
+<?php
+
+  include "../header.php";
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>CP Command</title>
+    <link rel="stylesheet" href="../../css/stylesheet.css">
+  </head>
+  <body>
+
+
+  </body>
+</html>
+
+
+
+
+
+
+<?php
+
+  include "../footer.php";
+
+ ?>
