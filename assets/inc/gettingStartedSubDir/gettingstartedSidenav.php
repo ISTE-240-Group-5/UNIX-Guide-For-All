e171351a2169e75c535a1e486eae9f1fd10f058b
zoryisnotreal <90271082+zoryisnotreal@users.noreply.github.com>
2021-11-22 23:13:30 -0500

    Create gettingstartedSidenav.php

---
+++ assets/inc/gettingStartedSubDir/gettingstartedSidenav.php
@@ -0,0 +1,7 @@
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="#"><?php echo $pageNameOne ?></a>
+  <a href="#"><?php echo $pageNameTwo ?></a>
+  <a href="#"><?php echo $pageNameThree ?></a>
+  <a href="#"><?php echo $pageNameFour ?></a>
+</div>
