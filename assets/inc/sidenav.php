79fc7ec9319e9c6ae6369bb15f11790a07fa265b
zoryisnotreal <90271082+zoryisnotreal@users.noreply.github.com>
2021-11-22 21:46:38 -0500

    Update sidenav.php

--- assets/inc/sidenav.php
+++ assets/inc/sidenav.php
@@ -1,7 +1,6 @@
 <div id="mySidenav" class="sidenav">
   <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-  <a href="#"><?php echo $pageNameOne ?></a>
-  <a href="#"><?php echo $pageNameTwo ?></a>
-  <a href="#"><?php echo $pageNameThree ?></a>
-  <a href="#"><?php echo $pageNameFour ?></a>
+  <a href="#"> What is Linux/Unix?</a>
+  <a href="#">How to use Unix?</a>
+  <a href="#">How to use Terminal?</a>
 </div>
