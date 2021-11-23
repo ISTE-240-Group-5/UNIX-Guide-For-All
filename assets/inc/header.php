14392e5c5ef4d7dd144937152d21595751073820
zoryisnotreal <90271082+zoryisnotreal@users.noreply.github.com>
2021-11-22 23:44:26 -0500

    Update header.php

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -11,27 +11,16 @@
             <a class="link" href="#">Directory Commands</a>
             <a class="link" href="#">File Commands</a>
             <a class="link" href="#">Advanced Commands</a>
-            <a class="link" href="#">Quizzes</a>
-
-        	 <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-   				 <i class="fa fa-bars"></i>
- 		    </a>
-            <a href="./assets/inc/gettingStarted.php">Getting Started</a>
-            <a href="#">Directory Commands</a>
-            <a href="#">File Commands</a>
-            <a href="#">Advanced Commands</a>
-            <a href="./assets/inc/quiz.php">Quizzes</a>
-
+            <a class="link" href="http://serenity.ist.rit.edu/~iste240t18/project-main/assets/inc/quiz.php">Quizzes</a>
 
                     <!-- search bar right align -->
-            <div class="search">
+           <div class="search">
                 <form action="#">
                 	<img class="searchButton" 	
 					src="https://img.icons8.com/material-outlined/24/ffffff/search--v1.png"/>
 				<input type="text" name="search">
                 </form>
+                
             </div>
-
-
         </div>
     </div>
