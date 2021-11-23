77eef750e83feb68a4839305419ae3084ea6608d
zoryisnotreal <90271082+zoryisnotreal@users.noreply.github.com>
2021-11-22 21:25:28 -0500

    SideNav

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,5 +1,10 @@
-function myFunction(x) {
-  x.classList.toggle("change");
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
 }
 
 
