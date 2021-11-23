15cf5c861886efdab19a7eccf07c8a70364bef2e
zoryisnotreal <90271082+zoryisnotreal@users.noreply.github.com>
2021-11-22 23:15:16 -0500

    Delete sidenav.php

--- assets/inc/sidenav.php
+++
@@ -1,6 +0,0 @@
-<div id="mySidenav" class="sidenav">
-  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-  <a href="#"> What is Linux/Unix?</a>
-  <a href="#">How to use Unix?</a>
-  <a href="#">How to use Terminal?</a>
-</div>
