bc682a8df49c025dcdf7101485f93ac83c432952
JulianMato <jmm5458@rit.edu>
2021-12-01 08:44:37 -0500

    fix file paths

--- assets/inc/global/nav.php
+++ assets/inc/global/nav.php
@@ -1,20 +1,22 @@
-<div class="topnav">
+ <div class="topnav">
+    	 <!-- <span class="link" style="font-size:20px; padding: 15px; cursor:pointer" onclick="openNav()">&#9776;</span> -->
+         
+            <a class="link <?php echo((isset($page) && $page=='gettingStarted')? ' active"' : ''); ?>   " href="assets/inc/get-start/gettingStarted.php">Getting Started</a>            
+            <a class="link <?php echo((isset($page) && $page=='dirCommands')? ' active"' : ''); ?>    " href="assets/inc/dir-com/dirCommands.php">Directory Commands</a>
+            <a class="link <?php echo((isset($page) && $page=='fileCommands')? ' active"' : ''); ?>   " href="assets/inc/file-com/fileCommands.php">File Commands</a>
+            <a class="link <?php echo((isset($page) && $page=='advancedCommands')? ' active"' : ''); ?>    " href="assets/inc/adv-com/advCommands.php">Advanced Commands</a>
+            <a class="link <?php echo((isset($page) && $page=='quizzes')? ' active"' : ''); ?>    " href="assets/inc/quiz/quiz.php">Quizzes</a>
 
-    <!-- <span class="link" style="font-size:20px; padding: 15px; cursor:pointer" onclick="openNav()">&#9776;</span> -->
+                    <!-- search bar right align -->
+            <div class="search">
+                <form action="#">
+                	<img class="searchButton" 	
+					src="https://img.icons8.com/material-outlined/24/ffffff/search--v1.png"/>
+				<input type="text" name="search">
+                </form>
+            </div>
 
-    <a class="link <?php echo((isset($page) && $page=='gettingStarted')? ' active"' : ''); ?>   " href="assets/inc/get-start/gettingStarted.php">Getting Started</a>            
-    <a class="link <?php echo((isset($page) && $page=='dirCommands')? ' active"' : ''); ?>    " href="assets/inc/dir-com/dirCommands.php">Directory Commands</a>
-    <a class="link <?php echo((isset($page) && $page=='fileCommands')? ' active"' : ''); ?>   " href="assets/inc/file-com/fileCommands.php">File Commands</a>
-    <a class="link <?php echo((isset($page) && $page=='advancedCommands')? ' active"' : ''); ?>    " href="assets/inc/adv-com/advCommands.php">Advanced Commands</a>
-    <a class="link <?php echo((isset($page) && $page=='quizzes')? ' active"' : ''); ?>    " href="assets/inc/quiz/quiz.php">Quizzes</a>
 
-    <!-- search bar right align -->
-    <div class="search">
-        <form action="#">
-            <img class="searchButton" src="https://img.icons8.com/material-outlined/24/ffffff/search--v1.png"/>
-            <input type="text" name="search">
-        </form>
+        </div>
     </div>
-
-</div>
 
