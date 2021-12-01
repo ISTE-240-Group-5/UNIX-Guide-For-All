cc000cbd30ca3e73f86cedd1e77670920d971dfb
JulianMato <jmm5458@rit.edu>
2021-12-01 08:45:33 -0500

    fix file paths

--- assets/inc/global/header.php
+++ assets/inc/global/header.php
@@ -1,6 +1,6 @@
-
 <div class="pageHeader">
-    <div class="whitebanner"></div>
+    <div class="whitebanner">
     <a href="index.php"><img class="logo" src="assets/images/small/navlogo.png" alt="company logo"></a> 
+        
 </div>
                
