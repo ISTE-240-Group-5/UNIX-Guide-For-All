d3c35d6e8aeab6634bfead379807d63e45a8c351
JulianMato <jmm5458@rit.edu>
2021-12-01 08:22:11 -0500

    nav file path fix

--- assets/inc/global/nav.php
+++ assets/inc/global/nav.php
@@ -1,11 +1,11 @@
  <div class="topnav">
     	 <!-- <span class="link" style="font-size:20px; padding: 15px; cursor:pointer" onclick="openNav()">&#9776;</span> -->
          
-            <a class="link <?php echo((isset($page) && $page=='gettingStarted')? ' active"' : ''); ?>   " href="../get-start/gettingStarted.php">Getting Started</a>            
-            <a class="link <?php echo((isset($page) && $page=='dirCommands')? ' active"' : ''); ?>    " href="../dir-com/dirCommands.php">Directory Commands</a>
-            <a class="link <?php echo((isset($page) && $page=='fileCommands')? ' active"' : ''); ?>   " href="../file-com/fileCommands.php">File Commands</a>
-            <a class="link <?php echo((isset($page) && $page=='advancedCommands')? ' active"' : ''); ?>    " href="../adv-com/advCommands.php">Advanced Commands</a>
-            <a class="link <?php echo((isset($page) && $page=='quizzes')? ' active"' : ''); ?>    " href="../quiz/quiz.php">Quizzes</a>
+            <a class="link <?php echo((isset($page) && $page=='gettingStarted')? ' active"' : ''); ?>   " href="assets/inc/get-start/gettingStarted.php">Getting Started</a>            
+            <a class="link <?php echo((isset($page) && $page=='dirCommands')? ' active"' : ''); ?>    " href="assets/inc/dir-com/dirCommands.php">Directory Commands</a>
+            <a class="link <?php echo((isset($page) && $page=='fileCommands')? ' active"' : ''); ?>   " href="assets/inc/file-com/fileCommands.php">File Commands</a>
+            <a class="link <?php echo((isset($page) && $page=='advancedCommands')? ' active"' : ''); ?>    " href="assets/inc/adv-com/advCommands.php">Advanced Commands</a>
+            <a class="link <?php echo((isset($page) && $page=='quizzes')? ' active"' : ''); ?>    " href="assets/inc/quiz/quiz.php">Quizzes</a>
 
                     <!-- search bar right align -->
             <div class="search">
