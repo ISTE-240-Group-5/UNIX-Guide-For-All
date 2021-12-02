5f896f15e28ce62595959f6995083eff8c9c369b
zoryisnotreal <90271082+zoryisnotreal@users.noreply.github.com>
2021-12-02 04:12:10 -0500

    poop

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -7,7 +7,7 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
-
+/**
 //Get the button
 	var mybutton = document.getElementById("myBtn");
 
@@ -27,16 +27,17 @@ function closeNav() {
  		 document.body.scrollTop = 0;
  		 document.documentElement.scrollTop = 0;
 	}
+**/
 	
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
-  document.getElementsByClass("content").style.marginLeft = "250px";
+  document.getElementsByClassName("content").style.marginLeft = "250px";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementsByClass("content").style.marginLeft = "0";
+  document.getElementsByClassName("content").style.marginLeft = "0";
 }
 
