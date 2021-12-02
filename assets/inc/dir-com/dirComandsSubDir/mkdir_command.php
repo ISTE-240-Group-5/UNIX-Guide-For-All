7e471362ef363902f0b48ae9a47fddb7e880c709
Joshua Kelly <jmk3378@rit.edu>
2021-12-02 06:16:08 -0500

    Created commnad pages (WIP).

---
+++ assets/inc/dir-com/dirComandsSubDir/mkdir_command.php
@@ -0,0 +1,30 @@
+<h3 class="subsubheader">mkdir Command</h3>
+
+<p class="text">creates a directory or directories with the specified names</p>
+
+  <h4 class="subsub" >Options</h4>
+
+  <p class="text">
+    -p: if the parent directories do not exist, create them
+  </p>
+
+
+
+
+<div class="terminals">
+<p> <span style="color:#00FF85">example.bot:</span><span style="color:#00F0FF">~ $</span>
+ cd </p>
+        </div>
+
+
+<p class="text">Syntax: mkdir [option] [directory]
+    [directory] can be a name or a filepath
+</p>
+
+<h4 class="subsub" >Example 1: mkdir -p quasar/example</h4>
+
+<p class="text">Creates a directory “quasar” with child directory “example”</p>
+
+
+
+</div>
