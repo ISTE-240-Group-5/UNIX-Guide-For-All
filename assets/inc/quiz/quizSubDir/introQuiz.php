159637d3a23e1eb7bf6d67f406d610ec81bdd0ef
mgrobe657 <mgrobe657@gmail.com>
2021-12-02 02:45:45 -0500

    fixed highlight?

--- assets/inc/quiz/quizSubDir/introQuiz.php
+++ assets/inc/quiz/quizSubDir/introQuiz.php
@@ -45,6 +45,16 @@ Rights       : Copyright (c) 2021 Quasar - GPLv3
     
     <script>
 
+    function highlight(text) {
+    var inputText = document.getElementById("inputText");
+    var innerHTML = inputText.innerHTML;
+    var index = innerHTML.indexOf(text);
+    if (index >= 0) { 
+    innerHTML = innerHTML.substring(0,index) + "<span class='highlight'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length);
+    inputText.innerHTML = innerHTML;
+    }
+    }
+
     function gradeQ1(){
         if(document.getElementById('1-4').checked) {
             .highlight {background-color: green;}
