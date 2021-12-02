cfa2923490d12eea13407b817e278ce9e8a6b2ec
mgrobe657 <mgrobe657@gmail.com>
2021-12-02 01:08:47 -0500

    intro quiz update

--- assets/inc/quiz/quizSubDir/introQuiz.php
+++ assets/inc/quiz/quizSubDir/introQuiz.php
@@ -11,35 +11,60 @@ Rights       : Copyright (c) 2021 Quasar - GPLv3
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Quasar Quizzez</title>
+    <title>Quasar Quizzes</title>
     <meta name="_____" content="_____">
     <meta name="_____" content="_____">
 
     <!-- Mobile-friendly viewport -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <link rel="stylesheet" href="../../css/stylesheet.css">
+    <link rel="stylesheet" href="../../../css/stylesheet.css">
 </head>
 <body>
     <?php 
         $path = './';
         //include header
-        require($path.'../header.php');
+        require($path.'../../global/header.php');
 
     ?>	
     <div class="quizPageMargins">
-    
+
+    <form name="question 1" onsubmit="return gradeQ1();">
+	<div id="q1" class="qdiv">
     <h1 class="header"> Introduction Quiz </h1>
     <h2 class="subheader">1. What is UNIX?</h2>
-	<div class="qdiv">
     <input class="qDivFont" type="radio" name ="1" value = "A system of mathematical equations" id="1-1"/><label for="1-1">A system of mathematical equations<label><br/>
 	<input class="qDivFont" type="radio" name ="1" value = "A multitasking and multifunctional operating system" id="1-2"/><label for="1-2">A multitasking and multifunctional operating system<label><br/>
 	<input class="qDivFont" type="radio" name ="1" value = "A server organizer"  id="1-3"/><label for="1-3">A server organizer<label><br/>
 	<input class="qDivFont" type="radio" name ="1" value = "All of the above"  id="1-4"/><label for="1-4">All of the above<label><br/>
-    <input type="submit"  name="Submit"  value="Submit Answer"  />
+    <input type="submit"  name="submit"  value="Submit"  />
     </div>
+    </form>
+
     </div>
+    
     <script>
+
+    function gradeQ1(){
+        if(document.getElementById('1-4').checked) {
+            .highlight {background-color: green;}
+        }else if(document.getElementById('1-1').checked) {
+            .highlight {background-color: red;}
+        }else if(document.getElementById('1-2').checked) {
+            .highlight {background-color: red;}
+        }else if(document.getElementById('1-3').checked) {
+            .highlight {background-color: red;}
+        }else{
+            echo("oh no");
+        }
+    }
+
+
+        function scrolldiv() {
+            var q1 = document.getElementById("q1");
+            q1.scrollIntoView();
+        }
+
 	//Get the button
 	var mybutton = document.getElementById("myBtn");
 
@@ -59,7 +84,7 @@ Rights       : Copyright (c) 2021 Quasar - GPLv3
  		 document.body.scrollTop = 0;
  		 document.documentElement.scrollTop = 0;
 	}
-</script>
+    </script>
 
     <?php 
 
