53e79d4865b7790a8ab5fe9e352895f337ad7f94
zoryisnotreal <90271082+zoryisnotreal@users.noreply.github.com>
2021-12-02 05:06:41 -0500

    fix sidenav

--- assets/inc/global/header.php
+++ assets/inc/global/header.php
@@ -3,4 +3,4 @@
     <a href="http://solace.ist.rit.edu/~iste240t18/project-dev/index.php"><img class="logo" src="http://solace.ist.rit.edu/~iste240t18/project-main/assets/images/small/navlogo.png" alt="company logo"></a> 
         
 </div>
-               
+        
