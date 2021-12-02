b2108e8abf91ceaf298ca63c225a78f6b2dcf61a
JulianMato <jmm5458@rit.edu>
2021-12-02 05:45:19 -0500

    quiz js

--- assets/inc/quiz/quizSubDir/introQuiz.php
+++ assets/inc/quiz/quizSubDir/introQuiz.php
@@ -19,34 +19,9 @@ Rights       : Copyright (c) 2021 Quasar - GPLv3
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <link rel="stylesheet" href="../../../css/stylesheet.css">
-</head>
-<body>
-    <?php 
-        $path = './';
-        //include header
-        require($path.'../../global/header.php');
-        require('../../global/nav.php');
-
-    ?>	
-    <div class="quizPageMargins">
-
-    <form name="question 1" onsubmit="return gradeQ1();">
-	<div id="q1" class="qdiv">
-    <h1 class="header"> Introduction Quiz </h1>
-    <h2 class="subheader">1. What is UNIX?</h2>
-    <input class="qDivFont" type="radio" name ="1" value = "A system of mathematical equations" id="1-1"/><label for="1-1">A system of mathematical equations</label><br/>
-	<input class="qDivFont" type="radio" name ="1" value = "A multitasking and multifunctional operating system" id="1-2"/><label for="1-2">A multitasking and multifunctional operating system</label><br/>
-	<input class="qDivFont" type="radio" name ="1" value = "A server organizer"  id="1-3"/><label for="1-3">A server organizer</label><br/>
-	<input class="qDivFont" type="radio" name ="1" value = "All of the above"  id="1-4"/><label for="1-4">All of the above</label><br/>
-    <input type="submit"  name="submit"  value="Submit" onclick="gradeQ1()"/>
-    </div>
-    </form>
-
-    </div>
-    
     <script>
 
-    function highlight(text) {
+    /*function highlight(text) {
     var inputText = document.getElementById("inputText");
     var innerHTML = inputText.innerHTML;
     var index = innerHTML.indexOf(text);
@@ -54,7 +29,7 @@ Rights       : Copyright (c) 2021 Quasar - GPLv3
     innerHTML = innerHTML.substring(0,index) + "<span class='highlight'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length);
     inputText.innerHTML = innerHTML;
     }
-    }
+    }*/
 
     function gradeQ1(){
         if(document.getElementById('1-1').checked) {
@@ -99,6 +74,32 @@ Rights       : Copyright (c) 2021 Quasar - GPLv3
  		 document.documentElement.scrollTop = 0;
 	}
     </script>
+</head>
+<body>
+    <?php 
+        $path = './';
+        //include header
+        require($path.'../../global/header.php');
+        require('../../global/nav.php');
+
+    ?>	
+    <div class="quizPageMargins">
+
+    <form name="question 1" onsubmit="return gradeQ1();">
+	<div id="q1" class="qdiv">
+    <h1 class="header"> Introduction Quiz </h1>
+    <h2 class="subheader">1. What is UNIX?</h2>
+    <input class="qDivFont" type="radio" name ="1" value = "A system of mathematical equations" id="1-1"/><label for="1-1">A system of mathematical equations</label><br/>
+	<input class="qDivFont" type="radio" name ="1" value = "A multitasking and multifunctional operating system" id="1-2"/><label for="1-2">A multitasking and multifunctional operating system</label><br/>
+	<input class="qDivFont" type="radio" name ="1" value = "A server organizer"  id="1-3"/><label for="1-3">A server organizer</label><br/>
+	<input class="qDivFont" type="radio" name ="1" value = "All of the above"  id="1-4"/><label for="1-4">All of the above</label><br/>
+    <input type="submit"  name="submit"  value="Submit" onclick="gradeQ1()"/>
+    </div>
+    </form>
+
+    </div>
+    
+    
 
     <?php 
 
