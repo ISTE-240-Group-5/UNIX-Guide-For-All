c65307a5267b1bf1d1e0ac59ab56f9a965176d09
zoryisnotreal <90271082+zoryisnotreal@users.noreply.github.com>
2021-12-02 07:22:25 -0500

    Update javascript.js

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -32,12 +32,14 @@ function closeNav() {
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
+  document.getElementsByClassName("content").style.marginLeft = "50px";
   
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.width = "0px";
+  document.getElementsByClassName("content").style.marginLeft = "0px";
   
 }
 
