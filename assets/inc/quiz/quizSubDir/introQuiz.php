d95a7ea9b89ab4f42eb9be6b07717d5b70df12ac
JulianMato <jmm5458@rit.edu>
2021-12-02 06:44:15 -0500

    quiz js

--- assets/inc/quiz/quizSubDir/introQuiz.php
+++ assets/inc/quiz/quizSubDir/introQuiz.php
@@ -34,7 +34,7 @@ Rights       : Copyright (c) 2021 Quasar - GPLv3
 	<div id="q1" class="qdiv">
     <h1 class="header"> Introduction Quiz </h1>
     <h2 class="subheader">1. What is UNIX?</h2>
-    <input class="qDivFont" type="radio" name ="1" value = "A system of mathematical equations" id="1-1"/><label id="a" for="1-1">A system of mathematical equations</label><br/>
+    <input class="qDivFont" type="radio" name ="1" value = "A system of mathematical equations" id="1-1"/><label id="a"  class="qDivFont" for="1-1">A system of mathematical equations</label><br/>
 	<input class="qDivFont" type="radio" name ="1" value = "A multitasking and multifunctional operating system" id="1-2"/><label for="1-2">A multitasking and multifunctional operating system</label><br/>
 	<input class="qDivFont" type="radio" name ="1" value = "A server organizer"  id="1-3"/><label for="1-3">A server organizer</label><br/>
 	<input class="qDivFont" type="radio" name ="1" value = "All of the above"  id="1-4"/><label for="1-4">All of the above</label><br/>
