f2548dd320bc53a3b7f9ab44a0832e07edc877b0
zoryisnotreal <90271082+zoryisnotreal@users.noreply.github.com>
2021-12-02 02:32:20 -0500

    things

--- assets/inc/global/nav.php
+++ assets/inc/global/nav.php
@@ -1,10 +1,10 @@
 <div class="topnav">
 
-    <span class="link" style="font-size:20px; padding: 15px; cursor:pointer" onclick="openNav()">&#9776;</span>
-    <a class="link <?php echo((isset($page) && $page=='gettingStarted')? ' active"' : ''); ?>   " href="solace.ist.rit.edu/~iste240t18/project-dev/assets/inc/get-start/gettingStarted.php">Getting Started</a>            
-    <a class="link <?php echo((isset($page) && $page=='dirCommands')? ' active"' : ''); ?>    " href="solace.ist.rit.edu/~iste240t18/project-dev/assets/inc/dir-com/dirCommands.php">Directory Commands</a>
-    <a class="link <?php echo((isset($page) && $page=='fileCommands')? ' active"' : ''); ?>   " href="solace.ist.rit.edu/~iste240t18/project-dev/assets/inc/file-com/fileCommands.php">File Commands</a>
-    <a class="link <?php echo((isset($page) && $page=='advancedCommands')? ' active"' : ''); ?>    " href="solace.ist.rit.edu/~iste240t18/project-dev/assets/inc/adv-com/advCommands.php">Advanced Commands</a>
+    <span class="hamburger" style="font-size:20px; padding: 15px; cursor:pointer" onclick="openNav()">&#9776;</span>
+    <a class="link <?php echo((isset($page) && $page=='gettingStarted')? ' active"' : ''); ?>   " href="http://solace.ist.rit.edu/~iste240t18/project-dev/assets/inc/get-start/gettingStarted.php">Getting Started</a>            
+    <a class="link <?php echo((isset($page) && $page=='dirCommands')? ' active"' : ''); ?>    " href="http://solace.ist.rit.edu/~iste240t18/project-dev/assets/inc/dir-com/dirCommands.php">Directory Commands</a>
+    <a class="link <?php echo((isset($page) && $page=='fileCommands')? ' active"' : ''); ?>   " href="http://solace.ist.rit.edu/~iste240t18/project-dev/assets/inc/file-com/fileCommands.php">File Commands</a>
+    <a class="link <?php echo((isset($page) && $page=='advancedCommands')? ' active"' : ''); ?>    " href="http://solace.ist.rit.edu/~iste240t18/project-dev/assets/inc/adv-com/advCommands.php">Advanced Commands</a>
     <a class="link <?php echo((isset($page) && $page=='quizzes')? ' active"' : ''); ?>    " href="solace.ist.rit.edu/~iste240t18/project-dev/assets/inc/quiz/quiz.php">Quizzes</a>
 
     <!-- search bar right align -->
@@ -16,4 +16,5 @@
     </div>
 
 </div>
+
 
